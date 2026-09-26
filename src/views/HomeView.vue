166253c6bfd787a62f4d2ba5c8f2631e68e055f7
IbrEmmanunel <iibrahime08140696953@gmail.com>
2026-09-26 18:43:50 +0000

    Hero: shift bg image to top (center 0%)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -936,7 +936,7 @@ onMounted(() => {
   inset: 0;
   z-index: 0;
   background-size: cover;
-  background-position: center 15%;
+  background-position: center 0%;
   background-repeat: no-repeat;
 }
 .hero-bg::after {
